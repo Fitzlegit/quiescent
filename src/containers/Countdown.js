1704d847264a18bb34d3637d205ed6c4e223508b
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-19 15:06:30 -0400

    created countdown container class

---
+++ src/containers/Countdown.js
@@ -0,0 +1,12 @@
+import React { Component } from 'react'
+import './App.css'
+
+class Countdown extends Component {
+  render() {
+    return (
+      <div className="countDown">
+        <div className="countDownHeader">Countdown</div>
+      </div>
+    )
+  }
+}
