cb4e40622c4fc47032b5816c7591836e86567444
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-19 23:30:25 -0400

    added timer reducer to root reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer'
 import profileReducer from './profileReducer'
-
+import timerReducer from './timerReducer'
 
 const rootReducer = combineReducers({
   users: userReducer,
-  profiles: profileReducer
+  profiles: profileReducer,
+  timers: timerReducer
 });
 
 export default rootReducer
