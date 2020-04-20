739ba51414e3740fcde4d8e2e6d8d71be91a35b9
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-20 01:48:03 -0400

    added select time reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import userReducer from './userReducer'
 import profileReducer from './profileReducer'
 import timerReducer from './timerReducer'
+import selectTimerReducer from './selectTimerReducer'
 
 const rootReducer = combineReducers({
   users: userReducer,
   profiles: profileReducer,
-  timers: timerReducer
+  timers: timerReducer,
+  selectedTimer: selectTimerReducer
 });
 
 export default rootReducer
