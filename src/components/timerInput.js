4f15d687ab319be0471e041192f319d2e38aca63
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-20 02:01:34 -0400

    added timer input

---
+++ src/components/timerInput.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { addTimer } from '../actions/timerActions'
+
+class TimerInput extends Component {
+  state = {
+    name: ""
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type='text'
+          placeholder='Meditation Timer Name'
+          name='name'
+          value={this.state.name}
+          onChange{(event) => this.setState({name: event.target.value})}/>
+          <button
+            onClick={(event) => {
+              this.props.addTimer(this.state.name)
+            }}>Save</button>
+      </div>
+    )
+  }
+}
