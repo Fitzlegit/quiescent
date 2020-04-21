2712e381f74032b210ae59d3e9693cdebd700d24
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-21 13:44:52 -0400

    removed files

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import rootReducer from './reducers/rootReducer'
+// import { createStore, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux'
+// import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+
+// const store = createStore(null, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
