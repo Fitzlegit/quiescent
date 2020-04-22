a917557c773061409952059d703510e2296fdbcb
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-22 10:03:40 -0400

    added reducer for user

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,20 @@
+const userReducer = (state = {users: [], loading: false}, actions) => {
+  switch(action.type) {
+    case 'LOADING_USERS':
+      return {
+        ...state,
+        users: [...state.users],
+        loading: true
+      }
+    case 'ADD_USERS'
+      return {
+        ...state,
+        users: action.users,
+        loading: false
+      }
+      default:
+        return state;
+  }
+}
+
+export default userReducer
