9ff9f52a4c23b1c88bac8845e323499e9b9ed811
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-22 09:45:22 -0400

    added onChange and onSubmit functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import './w3.css';
+import * as comp from './components'
 
 
 function App() {
   return (
     <div className="App w3-display-container w3-animate-opacity w3-text-white">
-      dfgdfdfdfd
+      <comp.UserInput />
     </div>
   );
 }
--- src/components/user/UserInput.js
+++ src/components/user/UserInput.js
@@ -3,24 +3,48 @@ import './User.css'
 
 class UserInput extends Component {
 
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault()
+    this.setState({
+      email: '',
+      password: ''
+    })
+  }
 
 
   render() {
     return (
       <div className="centered">
-        <form>
-          <h3>USERNAME</h3>
+        <form onSubmit={event => this.handleOnSubmit(event)}>
+          <h3>EMAIL</h3>
           <input
-            type="text"
+            type="email"
+            value={this.state.email}
+            name='email'
+            onChange={event => this.handleOnChange(event)}
             />
             <br/>
             <br/>
             <h4>PASSWORD</h4>
             <input
-              type="text"
+              type="password"
+              value={this.state.password}
+              name='password'
+              onChange={event => this.handleOnChange(event)}
               />
               <p/>
-            <input type="Submit" value="Login" />
+            <input type="Submit" value="Login/Register" />
         </form>
       </div>
     )
