395fd5dec16e97998466e382c6eea7eaa5b350b4
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-23 11:52:15 -0400

    added login logic

---
+++ src/containers/UserContainer.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import * as comp from '../components/user'
+class UserContainer extends Component {
+
+  componentDidMount() {
+    console.log(this.props)
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        // {this.props.current_user.id === undefined ? <comp.UserInput/> : <comp.UserHome current_user={this.props.current_user} /> }
+        <comp.UserHome current_user={this.props.current_user} />
+      </div>
+
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    current_user: state.current_user,
+  }
+}
+
+export default connect(mapStateToProps)(UserContainer)
