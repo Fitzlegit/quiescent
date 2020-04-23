5e9894e6493b0061c2b18e94eff9b4d850c06ed4
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-23 11:46:12 -0400

    create set user post fetch request

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -2,13 +2,29 @@ const URL = 'http://127.0.0.1:3000/users'
 
 export const fetchUsers = () => {
   return (dispatch) => {
-    dispatch( type: 'LOADING_USERS')
+    dispatch({ type: 'LOADING_USERS'})
     fetch(URL)
       .then(res => {
         return res.json()
       })
         .then(resJSON => {
-          dispatch({type 'ADD_USERS', users: resJSON })
+          dispatch({type: 'ADD_USERS', users: resJSON })
+      })
+  }
+}
+
+export const fetchUser = (user) => {
+  return (dispatch) => {
+    fetch(URL, {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({user: user})
+    })
+      .then(res => {
+        return res.json()
+      })
+        .then(resJSON => {
+          dispatch({type: 'SET_USER', user: resJSON })
       })
   }
 }
