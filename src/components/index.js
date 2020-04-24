0849b3ca6eca708aaea95af88c7034c8ad2a06e7
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-24 00:39:15 -0400

    added profile files to barrel

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,13 @@
+
+// export files from user in bulk
 export { UserInput } from "./user"
 export { UserHome } from "./user"
+
+// export files from timer folder in bulk
+export { TimerInput } from './timer'
+export { Timer } from './timer'
+
+
+// export files from profile folder in bulk
+export { ProfileInput } from './profile'
+export { Profile } from './profile'
