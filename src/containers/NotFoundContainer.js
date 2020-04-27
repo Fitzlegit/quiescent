8f313303cee341c63e018d4ff7d6ca13d7bbb4ae
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-27 08:17:40 -0400

    created and added 404 container

---
+++ src/containers/NotFoundContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as comp from '../components/misc'
+
+export default function NotFoundContainer() {
+  return (
+    <div>
+      <comp.NotFound />
+    </div>
+  );
+}
