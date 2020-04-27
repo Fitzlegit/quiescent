3b8f2586095c21c668f955b0b14136e556753d10
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-27 08:21:24 -0400

    house cleaning

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -5,16 +5,11 @@ import * as comp from '../components/user'
 
 class UserContainer extends Component {
 
-  componentDidMount() {
-    console.log(this.props)
-  }
-
   render() {
     console.log(this.props)
     return (
       <div>
         {this.props.current_user.id === undefined ? <comp.UserInput/> : <comp.UserHome current_user={this.props.current_user} /> }
-        <comp.UserHome current_user={this.props.current_user} />
       </div>
 
     )
