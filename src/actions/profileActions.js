7c64d221b182e398cfde0feacaa082952bb8fc26
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-27 08:18:19 -0400

    created profile actions

---
+++ src/actions/profileActions.js
@@ -0,0 +1,25 @@
+const URL = 'http://127.0.0.1:3000/profiles'
+
+// export const fetchProfile = (user) => {
+//   return (dispatch) => {
+//     fetch(URL, {
+//       method: "POST",
+//       headers: { 'Content-Type': 'application/json'},
+//       body: JSON.stringify({user: user})
+//     })
+//       .then(res => {
+//         return res.json()
+//       })
+//         .then(resJSON => {
+//           dispatch({type: 'SET_USER', user: resJSON })
+//       })
+//   }
+// }
+
+export const updateProfile = () => {
+  
+}
+
+export const saveProfile = () => {
+
+}
