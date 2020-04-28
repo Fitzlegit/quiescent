5148bf92679380cc71eb5118fc1bf390185a7f41
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-28 14:09:51 -0400

    tried logic with onClick for profile link

--- src/components/misc/navbar.js
+++ src/components/misc/navbar.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
-import UserContainer from '../../containers/UserContainer'
 import { Link } from 'react-router-dom';
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import { LinkContainer as Route } from "react-router-bootstrap";
 import Routes from '../../Routes';
 import lotus from '../../img/lotus.svg'
-import * as comp from '../../components/user'
 import { connect } from 'react-redux'
+import { clearUser } from './../../actions/UserActions'
+import { fetchProfile } from '../../actions/profileActions'
 
 class Navigation extends Component {
 
   render() {
+
+  const isLoggedin = [
+    <Route to="/">
+      <Nav.Link>Home</Nav.Link>
+    </Route>,
+    <Route to="/profile">
+      <Nav.Link>Profile</Nav.Link>
+    </Route>,
+    <Route to="/logout">
+      <Nav.Link onClick={() => this.props.clearUser()}>Logout</Nav.Link>
+    </Route>
+  ]
+
     return (
         <div>
           <container>
-          <Navbar bg="dark" expand="md" className="sticky-top">
+          <Navbar bg="dark" variant='dark' expand="md" className="sticky-top">
             <Navbar.Brand>
               <img
                 className='d-inline-block align-top'
@@ -26,18 +39,13 @@ class Navigation extends Component {
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className=" justify-content-end">
-                <Route to="/profile">
-                  <Nav.Link>Profile</Nav.Link>
-                </Route>
+              <Nav className=" justify-content-end link-text">
                 { this.props.current_user.id === undefined ?
                   <Route to="/login">
                     <Nav.Link>Login</Nav.Link>
                   </Route>
                   :
-                  <Route to="/logout">
-                    <Nav.Link>Logout</Nav.Link>
-                  </Route>
+                  isLoggedin
               }
               </Nav>
             </Navbar.Collapse>
@@ -51,9 +59,9 @@ class Navigation extends Component {
 
 const mapStateToProps = state => {
   return {
-    current_user: state.current_user
+    current_user: state.user.current_user
   }
 }
 
 
-export default connect(mapStateToProps)(Navigation);
+export default connect(mapStateToProps, { clearUser })(Navigation);
