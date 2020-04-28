58ba8d35a70b558f41f8195a5f498e8a4c9f632f
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-28 14:07:13 -0400

    added additional dispatch action to set user profile on login

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -1,18 +1,5 @@
 const URL = 'http://127.0.0.1:3000/users'
 
-export const fetchUsers = () => {
-  return (dispatch) => {
-    dispatch({ type: 'LOADING_USERS'})
-    fetch(URL)
-      .then(res => {
-        return res.json()
-      })
-        .then(resJSON => {
-          dispatch({type: 'ADD_USERS', users: resJSON })
-      })
-  }
-}
-
 export const fetchUser = (user) => {
   return (dispatch) => {
     fetch(URL, {
@@ -25,6 +12,13 @@ export const fetchUser = (user) => {
       })
         .then(resJSON => {
           dispatch({type: 'SET_USER', user: resJSON })
+          dispatch({type: 'SET_PROFILE', profile: resJSON.profile })
       })
+  }
+}
+
+export const clearUser = () => {
+  return {
+    type: 'CLEAR_USER'
   }
 }
