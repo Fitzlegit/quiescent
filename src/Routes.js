28498a3dfbdc830a9b175ec62b3133dbfca6cfa0
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-28 14:12:19 -0400

    routed component UserHome to path /

--- src/Routes.js
+++ src/Routes.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom'
 import UserContainer from './containers/UserContainer'
 import ProfileContainer from './containers/ProfileContainer'
 import NotFoundContainer from './containers/NotFoundContainer'
-
+import { UserHome } from './components/user'
 
 export default function Routes() {
   return (
     <Switch>
+      <Route exact path='/' component={UserHome} />
+      <Route exact path='/logout' component={UserContainer} />
       <Route exact path='/login' component={UserContainer} />
       <Route exact path='/profile' component={ProfileContainer} />
       <Route component={NotFoundContainer} />
