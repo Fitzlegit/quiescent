2322d4be2a85a621ae4444da9a085cac8eb1d81c
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-28 13:58:18 -0400

    added footer component

---
+++ src/components/misc/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <a>{"\u00A9"} Quiescent 2020</a>
+    </div>
+  )
+}
+
+
+export default Footer;
