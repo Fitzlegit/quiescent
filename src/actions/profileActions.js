3106431a67036fc0246d50dc142d8f7efd5fed87
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-28 14:06:03 -0400

    added fetch to save profile

--- src/actions/profileActions.js
+++ src/actions/profileActions.js
@@ -16,10 +16,41 @@ const URL = 'http://127.0.0.1:3000/profiles'
 //   }
 // }
 
+
+// export const fetchProfile = (profile) => {
+//   return (dispatch) => {
+//     fetch(URL, {
+//       method: 'POST',
+//       headers: { 'Content-type' : 'application/json'},
+//       body: JSON.stringify({profile: profile})
+//     })
+//       .then(res => {
+//         return res.json()
+//       })
+//       .then(resJSON => {
+//         dispatch({type: 'FIND_PROFILE', profile: resJSON })
+//       })
+//   }
+// }
+
+
+
 export const updateProfile = () => {
-  
+
 }
 
-export const saveProfile = () => {
-
+export const saveProfile = (profile) => {
+  return (dispatch) => {
+    fetch(URL, {
+      method: 'POST',
+      headers: { 'Content-type' : 'application/json'},
+      body: JSON.stringify({profile: profile})
+    })
+      .then(res => {
+        return res.json()
+      })
+      .then(resJSON => {
+        dispatch({type: 'SAVE_PROFILE', profile: resJSON })
+      })
+  }
 }
