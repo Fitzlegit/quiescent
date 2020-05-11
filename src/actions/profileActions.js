8e4bd7d5a5fee972651fb052993a89301babd188
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-05-11 15:02:12 -0400

    corrected typo on URL const

--- src/actions/profileActions.js
+++ src/actions/profileActions.js
@@ -1,42 +1,19 @@
 const URL = 'http://127.0.0.1:3000/profiles'
 
-// export const fetchProfile = (user) => {
-//   return (dispatch) => {
-//     fetch(URL, {
-//       method: "POST",
-//       headers: { 'Content-Type': 'application/json'},
-//       body: JSON.stringify({user: user})
-//     })
-//       .then(res => {
-//         return res.json()
-//       })
-//         .then(resJSON => {
-//           dispatch({type: 'SET_USER', user: resJSON })
-//       })
-//   }
-// }
-
-
-// export const fetchProfile = (profile) => {
-//   return (dispatch) => {
-//     fetch(URL, {
-//       method: 'POST',
-//       headers: { 'Content-type' : 'application/json'},
-//       body: JSON.stringify({profile: profile})
-//     })
-//       .then(res => {
-//         return res.json()
-//       })
-//       .then(resJSON => {
-//         dispatch({type: 'FIND_PROFILE', profile: resJSON })
-//       })
-//   }
-// }
-
-
-
-export const updateProfile = () => {
-
+export const updateProfile = (profile) => {
+  return (dispatch) => {
+    fetch(URL, {
+      method: 'PATCH',
+      headers: { 'Content-type' : 'application/json' },
+      body: JSON.stringify({profile: profile})
+    })
+      .then(res => {
+        return res.json()
+      })
+      .then(resJSON => {
+        dispatch({type: 'UPDATE_PROFILE', profile: resJSON })
+      })
+  }
 }
 
 export const saveProfile = (profile) => {
