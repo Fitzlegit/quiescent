811840019f7a6f0bd80d572f62285ceea17aba48
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-07-01 22:40:35 -0400

    added ternary check for current_user

--- src/containers/ProfileContainer.js
+++ src/containers/ProfileContainer.js
@@ -1,34 +1,31 @@
-import React, { Component } from 'react'
-import * as comp from '../components/profile'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import * as comp from "../components/profile";
+import { connect } from "react-redux";
 
 class ProfileContainer extends Component {
-
-
-
   render() {
-
     return (
       <div>
-        {
-          this.props.current_user.id === undefined ? null :
-          (this.props.current_profile === undefined) ?
-          <comp.ProfileInput current_user={this.props.current_user}/> :
-          <comp.Profile key={this.props.current_profile.id} current_profile={this.props.current_profile}/>
-        }
-
+        {this.props.current_user.id &&
+        this.props.current_user === undefined ? null : this.props
+            .current_profile.id === undefined ? (
+          <comp.ProfileInput current_user={this.props.current_user} />
+        ) : (
+          <comp.Profile
+            key={this.props.current_profile.id}
+            current_profile={this.props.current_profile}
+          />
+        )}
       </div>
-    )
+    );
   }
 }
 
-
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     current_user: state.user.current_user,
-    current_profile: state.profile.current_profile
-  }
-}
-
+    current_profile: state.profile.current_profile,
+  };
+};
 
 export default connect(mapStateToProps)(ProfileContainer);
